c1b40c2330271fc2616fa2e9f91b3efe5eaed102
Ryan <ryan__mckenzie@hotmail.com>
2024-09-15 02:58:11 -0700

    check all features needed to run tests

--- crates/aob/src/lib.rs
+++ crates/aob/src/lib.rs
@@ -103,7 +103,7 @@ mod tests {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     #[test]
     fn test_avx2() {
-        if !is_x86_feature_detected!("avx2") {
+        if !is_x86_feature_detected!("avx") || !is_x86_feature_detected!("avx2") {
             return;
         }
 
